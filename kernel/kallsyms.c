fa00c15f6a86e45799239e55577c5733a5ca89e3
NyxNoirXD <luna@snowluna.eu.org>
2025-08-25 10:55:35 +0530

    susfs fix

--- kernel/kallsyms.c
+++ kernel/kallsyms.c
@@ -616,7 +616,7 @@ static int s_show(struct seq_file *m, void *p)
 		if (strstr(iter->name, "ksu_") || !strncmp(iter->name, "susfs_", 6) || !strncmp(iter->name, "ksud", 4)) {
 			return 0;
 		}
-		seq_printf(m, "%px %c %s\n", value,
+		seq_printf(m, "%px %c %s\n", (void *)iter->value,
 			   iter->type, iter->name);
 	}
 #endif
